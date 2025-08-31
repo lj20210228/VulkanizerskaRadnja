81659ae55b956991da567d56a850011871efe084
lazar-jankovic <lazarj1007@gmail.com>
2025-08-31 12:56:41 +0200

    Izmenjene metode, tako da im se mogu proslediti null vrednosti

--- app/src/main/java/com/example/vulkanizerskaradnja/autoRadnja/Radnja.kt
+++ app/src/main/java/com/example/vulkanizerskaradnja/autoRadnja/Radnja.kt
@@ -3,8 +3,8 @@ package com.example.vulkanizerskaradnja.autoRadnja
 import com.example.vulkanizerskaradnja.auto_radnja.gume.AutoGuma
 
 interface Radnja {
-    fun dodajGumu(guma: AutoGuma)
-    fun pronadjiGumu(markaModel:String):List<AutoGuma>
+    fun dodajGumu(guma: AutoGuma?)
+    fun pronadjiGumu(markaModel:String?):List<AutoGuma>
     fun vratiSveGume():List<AutoGuma>
 
 }
