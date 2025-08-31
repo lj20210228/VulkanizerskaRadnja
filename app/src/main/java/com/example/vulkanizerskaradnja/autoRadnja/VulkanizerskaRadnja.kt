e2ecb271dd7e2452ee81ce54244e986e134dfc7d
lazar-jankovic <lazarj1007@gmail.com>
2025-08-31 13:18:47 +0200

    Dodata dokumentacija za Radnja i VulkanizerskaRadnja

--- app/src/main/java/com/example/vulkanizerskaradnja/autoRadnja/VulkanizerskaRadnja.kt
+++ app/src/main/java/com/example/vulkanizerskaradnja/autoRadnja/VulkanizerskaRadnja.kt
@@ -2,8 +2,18 @@ package com.example.vulkanizerskaradnja.autoRadnja
 
 import com.example.vulkanizerskaradnja.auto_radnja.gume.AutoGuma
 
+/**
+ * Klasa koja implementira metode interfejsa [Radnja]
+ *
+ * @property lista Lista za test u koju ce se dodavati gume u kojoj cemo traziti gume
+ */
 class VulkanizerskaRadnja:Radnja {
+
     private var lista= mutableListOf<AutoGuma>()
+
+    /**
+     * Metoda za dodavanje gume
+     */
     override fun dodajGumu(guma: AutoGuma?) {
         if (guma==null)
             throw NullPointerException("Guma za dodavanje ne moze biti null")
@@ -14,6 +24,10 @@ class VulkanizerskaRadnja:Radnja {
         lista.add(guma)
 
     }
+
+    /**
+     * Metoda za pronalazenje gume po markaModel atributu
+     */
 
     override fun pronadjiGumu(markaModel: String?): List<AutoGuma> {
 
@@ -31,6 +45,9 @@ class VulkanizerskaRadnja:Radnja {
     }
 
 
+    /**
+     * Metoda za vracanje svih guma u listi
+     */
     override fun vratiSveGume(): List<AutoGuma> {
         return lista
     }
