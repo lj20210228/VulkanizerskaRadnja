bced47c5eb5492b377b8dbdd0889b86dbc8ed4ac
lazar-jankovic <lazarj1007@gmail.com>
2025-08-31 12:54:54 +0200

    Promenjena putanja do Radnja

---
+++ app/src/main/java/com/example/vulkanizerskaradnja/autoRadnja/Radnja.kt
@@ -0,0 +1,10 @@
+package com.example.vulkanizerskaradnja.autoRadnja
+
+import com.example.vulkanizerskaradnja.auto_radnja.gume.AutoGuma
+
+interface Radnja {
+    fun dodajGumu(guma: AutoGuma)
+    fun pronadjiGumu(markaModel:String):List<AutoGuma>
+    fun vratiSveGume():List<AutoGuma>
+
+}
--- app/src/main/java/com/example/vulkanizerskaradnja/auto_radnja/gume/Radnja.kt
+++
@@ -1,8 +0,0 @@
-package com.example.vulkanizerskaradnja.auto_radnja.gume
-
-interface Radnja {
-    fun dodajGumu(guma:AutoGuma)
-    fun pronadjiGumu(markaModel:String):List<AutoGuma>
-    fun vratiSveGume():List<AutoGuma>
-
-}
